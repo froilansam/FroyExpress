574b5241442d531b1230881e5f4a6cc5a7ffedc5
Froilan Sam Malibiran <froilansam@gmail.com>
2019-08-08 00:43:08 -0700

    Revert "For Improvement"
    This reverts commit 3b608426f5c6c790a645274b40be5d5c30d29874.

--- app/modules/main/route.js
+++ app/modules/main/route.js
@@ -1,158 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var logInRouter = express.Router();
-var appRouter = express.Router();
-var firebase = require('firebase');
-const nodemailer = require('nodemailer');   
-var config = {
-  apiKey: 'AIzaSyAwo-v5EvPL3VMsc9yuVOaTSrphpVERMgw',
-  authDomain: 'emailapp-65706.firebaseapp.com',
-  databaseURL: 'emailapp-65706.firebaseio.com',
-  projectId: 'emailapp-65706',
-  storageBucket: 'emailapp-65706.appspot.com',
-  messagingSenderId: '919686798595'
-}
-firebase.initializeApp(config);
-var database = firebase.firestore();
-let recipients = database.collection('recipients');
+var authMiddleware = require('../auth/middleware');
+var db = require('../../lib/database')();
 
-appRouter.get('/content', (req, res) => {
-    console.log('Log In Routes');
 
-    res.render('main/views/content');
-})
-
-appRouter.get('/', (req, res) => {
+logInRouter.use(authMiddleware.noAuthed);
+logInRouter.get('/', (req, res) => {
     console.log('Log In Router');
-    let numberProcessed = 0;
-    let arrayRecipients = [];
-    recipients.get().then(collection => {
-        console.log('----Collection: Recipients----')
-        let collectionLength = collection.size;
-        collection.forEach(doc => {
-            numberProcessed++
-            console.log(doc.data().fullname);
-            let recipientJSON = {}
-            recipientJSON['fullname'] = doc.data().fullname;
-            recipientJSON['emailaddress'] = doc.data().emailaddress;
-            recipientJSON['id'] = doc.id;
-            arrayRecipients.push(recipientJSON);
-            if(numberProcessed == collectionLength){
-                console.log(arrayRecipients)
-                res.render('main/views/recipients', {recipients: arrayRecipients});
-            }
-        });
-    });
-
+    res.render('main/views/login', req.query);
 })
+.post('/', (req, res) => {
 
-appRouter.post('/recipient/add', (req, res) => {
-    recipients.add({
-        fullname: req.body.name,
-        emailaddress: req.body.email,
-    })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-        res.send({docID: docRef.id});
-        res.end();
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
+
+    // Change the database name, and the table where you get the user.
+    var queryString = `SELECT * FROM users WHERE strEmail = ? AND strPassword = ?;`
+    db.query(queryString, [req.body.email, req.body.password], (err, results, fields) => {
+        if (err) throw err;
+        if (results.length === 0) return res.redirect('/login?incorrect');
+
+        var user = results[0];
+
+        delete user.password;
+
+        req.session.user = user;
+
+        return res.redirect('/index');
     });
 })
 
-appRouter.post('/delete', (req, res) => {
-    recipients.doc(req.body.docID).delete().then(function() {
-        console.log("Document successfully deleted!");
-        res.send('success');
-        res.end();
-    }).catch(function(error) {
-        console.error("Error removing document: ", error);
-        res.send('error');
-        res.end();
-    });
-})
-
-appRouter.post('/email/send', (req, res) => {
-    console.log(req.body.message)
-    var message = req.body.message;
-    var subject = req.body.subject;
-
-    function replaceAll(str, find, replace) {
-        return str.replace(new RegExp(find, 'g'), replace);
+router.use(authMiddleware.hasAuth);
+router.get('/', (req, res) => {
+    console.log('/index directory');
+    if (typeof process.env.ENABLE_DATABASE !== 'undefined' && process.env.ENABLE_DATABASE === 'false') {
+        /**
+         * If the database part is disabled, then pass a blank array to the
+         * render function.
+         */
+        return render([]);
     }
+    res.end();
+});
 
-    recipients.get().then(collection => {
-        console.log('----Collection: Recipients----')
-        collection.forEach(doc => {
-            let recipientJSON = {}
-            recipientJSON['fullname'] = doc.data().fullname;
-            recipientJSON['emailaddress'] = doc.data().emailaddress;
-            recipientJSON['id'] = doc.id;
-            var replacedMessage = replaceAll(message, "{{fullname}}", recipientJSON.fullname);
-            var replacedSubject = replaceAll(subject, "{{fullname}}", recipientJSON.fullname);
-            console.log(replacedMessage)
-
-            nodemailer.createTestAccount((err, account) => {
-
-                // create reusable transporter object using the default SMTP transport
-                let transporter = nodemailer.createTransport({
-                    host: 'smtp.gmail.com.',
-                    port: 587,
-                    secure: false, // true for 465, false for other ports
-                    auth: {
-                        user: 'shinaydalla@gmail.com',
-                        pass: 'aprilann49'
-                    },
-                    tls:{
-                        rejectUnauthorized: false
-                    }
-                });
-
-                mailOptions = {
-                    from: '"IDEATION PHILIPPINES" <contact@ideation.com>', // sender address
-                    to: recipientJSON.emailaddress, // list of receivers
-                    subject: replacedSubject, // Subject line
-                    html: replacedMessage,
-                };
-                console.log(mailOptions)
-
-                // send mail with defined transport object
-                transporter.sendMail(mailOptions, (error, info) => {
-                    if (error) {
-                        return console.log(error);
-                    }
-                    console.log('Message sent: %s', info.messageId);
-                    // Preview only available when sending through an Ethereal account
-                    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-                    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-                    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-                });
-            });
-
-
-        });
-    });
-
-    res.end()
-})
-
-appRouter.post('/email/save', (req, res) => {
-    console.log(req.body.message)
-    var message = req.body.message;
-    var contentTitle = req.body.contentTitle;
-
-    
-
-    
-
-    res.end()
-})
-
-appRouter.get('/email', (req, res) => {
-    console.log('Log In Router');
-    res.render('main/views/email');
-})
-
-exports.index = appRouter;
+exports.index = router;
 exports.login = logInRouter;
